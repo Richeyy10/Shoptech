d3bf5ee8788899b2353be35af109d911f624e79e
Righteousness <thericheyy@gmail.com>
2025-10-08 10:19:18 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar() {
                                 </UserButton.MenuItems>
                             </UserButton> 
                         </> 
-                    : <button onClick={()=> {openSignIn} } className="flex items-center gap-2 hover:text-gray-900 transition">
+                    : <button onClick={()=> openSignIn() } className="flex items-center gap-2 hover:text-gray-900 transition">
                         <Image src={assets.user_icon} alt="user icon" />
                         Account
                     </button>}
@@ -101,7 +101,7 @@ export default function Navbar() {
                                 </UserButton.MenuItems>
                             </UserButton> 
                         </> 
-                        : <button onClick={()=> {openSignIn}} className="flex items-center gap-2 hover:text-gray-900 transition">
+                        : <button onClick={()=> openSignIn()} className="flex items-center gap-2 hover:text-gray-900 transition">
                         <Image src={assets.user_icon} alt="user icon" />
                         Account
                     </button>}
